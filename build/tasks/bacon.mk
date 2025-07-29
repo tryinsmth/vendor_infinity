21c5f83ddfde05ecc4e833c53012cfcaf9b8eb88
tryinsmth <3mksuss1@gmail.com>
2025-07-29 22:27:38 +0300

    Update bacon.mk

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -16,7 +16,7 @@
 # -----------------------------------------------------------------
 # Infinity OTA update package
 
-INFINITY_TARGET_PACKAGE := $(PRODUCT_OUT)/$(INFINITY_VERSION).zip
+INFINITY_TARGET_PACKAGE := $(PRODUCT_OUT)/$(lineage_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
